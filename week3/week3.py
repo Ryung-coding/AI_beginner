d4d1ca817231244c676189cc2d7335041464052a
ryung <ryung9514@naver.com>
2025-11-14 00:44:47 +0900

    write add option meaning

--- week3/week3.py
+++ week3/week3.py
@@ -16,6 +16,16 @@ x_train, x_test, y_train, y_test = train_test_split(digit.data,digit.target,trai
 # input data(8X8 image) -> hidden_layer_sizes (expend) -> output1 -> last_layer_size = output_class_number / in out case = 10
 #          64                       100                     100                     10                        =>64*100*100*10 
 mlp=MLPClassifier(hidden_layer_sizes=(100), learning_rate_init=0.001,batch_size=32, max_iter=300, solver='sgd', verbose=True)
+hidden_layer_sizes=(100)
+
+# hidden_layer_sizes=(100)  ==> 은닉층 1개, 뉴런 100개
+# learning_rate_init=0.001  ==> 초기 학습률(η). solver='sgd'나 'adam'에서 사용. 너무 크면 발산, 너무 작으면 수렴 느림.
+# batch_size=32             ==> 한 번의 가중치 업데이트에 쓰는 샘플 수. solver='sgd'/'adam'에서만 의미 있음.
+# max_iter=300              ==> 에폭 수(전체 데이터셋을 1회 도는 횟수). 조기 종료가 없으면 최대 300에폭 학습.
+# solver='sgd'              => 'sgd': 확률적 경사하강법(+옵션으로 모멘텀/학습률 스케줄).
+                                # Note* 'adam': 보통 기본값 | 'lbfgs': 소규모 데이터에서 빠르게 수렴
+# verbose=True              ==> loss/상태를 로그로 출력.
+
 mlp.fit(x_train,y_train)
 
 result_prediction = mlp.predict(x_test)
